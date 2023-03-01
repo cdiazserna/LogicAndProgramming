0baca29b0dc0fa84fe00c3a10cae3eea88f3e246
Carlos Diaz <junior.diaz@selazar.com>
2023-02-28 19:19:05 -0500

    Nuevo programa ciclo for para hallar el factorial de un numero

---
+++ CIcloFor_Factorial/CIcloFor_Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CIcloFor_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desarrolle un algoritmo que pida por pantalla N números y calcule el factorial de ellos.
+
+            double n, i, factorial = 1;
+
+            Console.Write("Digite el número factorial: ");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("El factorial de: " + n + " es: " + factorial);
+        }
+    }
+}
