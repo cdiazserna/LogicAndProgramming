fa164e3c5f154e0d0b40d08b4eeea1f356fa9733
Carlos Diaz <junior.diaz@selazar.com>
2023-03-02 19:12:40 -0500

    Ejercicio 14 Ciclo For con Num Primos

---
+++ CicloFor/Ejercicio14_CicloFor/Ejercicio14_CicloFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio14_CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Un algoritmo que permita mostrar los números primos existentes entre 1 y N
+
+            int numbers, i, j;
+            bool isPrime = true;
+
+            Console.Write("Ingrese un tope: "); //6
+            numbers = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 2; i <= numbers; i++) //i = 4
+            {
+                isPrime = true;
+
+                for (j = 2; j < i; j++) //i = 4, j=2
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine($"El número {i} es primo");
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
